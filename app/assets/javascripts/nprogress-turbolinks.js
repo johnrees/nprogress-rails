5b8c2bc5a0aac937561072e860a109dcfcb186a7
John Rees <john@bitsushi.com>
2015-05-11 16:39:25 +0100

    more events

--- app/assets/javascripts/nprogress-turbolinks.js
+++ app/assets/javascripts/nprogress-turbolinks.js
@@ -3,4 +3,9 @@ jQuery(function() {
   jQuery(document).on('page:receive', function() { NProgress.set(0.7); });
   jQuery(document).on('page:change',  function() { NProgress.done();   });
   jQuery(document).on('page:restore', function() { NProgress.remove(); });
+
+  jQuery(document).on('turbograft:remote:start',   function() { NProgress.start();  });
+  jQuery(document).on('turbograft:remote:always', function() { NProgress.set(0.7); });
+  jQuery(document).on('turbograft:remote:success',  function() { NProgress.done();   });
+  jQuery(document).on('turbograft:remote:success', function() { NProgress.remove(); });
 });
